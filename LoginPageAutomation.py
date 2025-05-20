4cb69c58bc8c3c6d4f88b8ce62df9716b1c77f91
Wojciech Paradziński <84313530+Qxver@users.noreply.github.com>
2025-05-20 19:43:38 +0200

    Testing for invalid and valid user data

--- LoginPageAutomation.py
+++ LoginPageAutomation.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
@@ -12,7 +11,7 @@ driver.implicitly_wait(10)
 
 driver.get("https://the-internet.herokuapp.com")  # Website
 
-WebDriverWait(driver, 5).until(
+WebDriverWait(driver, 10).until(
     EC.presence_of_element_located((By.LINK_TEXT, "Form Authentication"))
 )
 
@@ -21,11 +20,33 @@ link.click()
 
 user_input = driver.find_element(By.ID, "username")
 user_input.clear()
+user_input.send_keys("RandomUsername")
+
+password_input = driver.find_element(By.ID, "password")
+password_input.clear()
+password_input.send_keys("RandomPassword")
+
+driver.find_element(By.CLASS_NAME, "radius").click()
+
+WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.CLASS_NAME, "flash.error"))
+)
+
+user_input = driver.find_element(By.ID, "username")
+user_input.clear()
 user_input.send_keys("tomsmith")
 
 password_input = driver.find_element(By.ID, "password")
 password_input.clear()
-password_input.send_keys("SuperSecretPassword!" + Keys.ENTER)
+password_input.send_keys("SuperSecretPassword!")
+
+driver.find_element(By.CLASS_NAME, "radius").click()
+
+WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.CLASS_NAME, "flash.success"))
+)
+
+driver.find_element(By.CLASS_NAME, "button.secondary.radius").click()
 
 time.sleep(20)
 driver.quit()
